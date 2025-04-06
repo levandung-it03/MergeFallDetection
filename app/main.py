c0404658a5fc68a016fd7c8afff6fa2179e42891
Dat Nguyen <=>
2025-04-06 17:08:42 +0700

    feat: Add CameraRoutes

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.app_sql.setup_database import Base, engine
 from app.req_filters.AuthInterceptor import AuthInterceptor
 from fastapi import FastAPI
-from app.routes import AccountRoutes, DetectionRoutes
+from app.routes import AccountRoutes, DetectionRoutes, CameraRoutes
 
 
 # Initialize data
@@ -15,10 +15,14 @@ async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     yield
 
+origins = [
+    "http://localhost:3000",  
+]
+
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "PUT", "POST", "DELETE", "OPTIONS"],
     allow_headers=["*"],
@@ -30,3 +34,4 @@ authInterceptor.turn_on()
 # Include routers
 app.include_router(AccountRoutes.router)
 app.include_router(DetectionRoutes.router)
+app.include_router(CameraRoutes.router)
